0dfbddca00cfe55b7538c8c0834cb879a6056233
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2018-12-21 00:18:59 +0700

    edit

--- webhooks.php
+++ webhooks.php
@@ -117,7 +117,7 @@ else if($message == "ฝันดี"){
 else if($message == "userID"){
 	$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
 	$arrayPostData['messages'][0]['type'] = "text";
-	$arrayPostData['messages'][0]['text'] = 'userID';
+	$arrayPostData['messages'][0]['text'] = 'userId';
 	replyMsg($arrayHeader,$arrayPostData);
 }
 
