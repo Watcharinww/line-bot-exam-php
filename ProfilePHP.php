d6e70b4db1a16bc73381d51871ecdffc9f0bdb3e
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-01-01 15:13:36 +0700

    edit push and add profile

---
+++ ProfilePHP.php
@@ -0,0 +1,22 @@
+<?php
+
+require "vendor/autoload.php";
+
+$access_token = 'UoNWCzm+34uMMB2itvPBwm7K9N7CK8GbWMc5RFmI9KQqGtAM1YO24VRTp5xTbzYk4jN9n0zEqc86nVJQTyIVJOimoI9CPzcuaCUyysOLMvBtooxc7BK6pfNYdRZ6mzobVVvb7/DlYxK/LdHddOHrrwdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = '88f693bafb5809e65f319ad3139213ba';
+
+$userID = 'U1b80d09ffe5c7f746850ca99a023d30b';
+
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret ]);
+$response = $bot->getProfile($userID);
+if ($response->isSucceeded()) {
+    $profile = $response->getJSONDecodedBody();
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
+}
+
+?>
--- botpush.php
+++ botpush.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 require "vendor/autoload.php";
 
 $access_token = 'UoNWCzm+34uMMB2itvPBwm7K9N7CK8GbWMc5RFmI9KQqGtAM1YO24VRTp5xTbzYk4jN9n0zEqc86nVJQTyIVJOimoI9CPzcuaCUyysOLMvBtooxc7BK6pfNYdRZ6mzobVVvb7/DlYxK/LdHddOHrrwdB04t89/1O/w1cDnyilFU=';
@@ -18,9 +16,4 @@ $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
-
-
-
-
-
-
+?>
