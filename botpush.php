8c2aa5fcc5ebc883b4f9a858dfb7aa6d33b374a8
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-01-08 13:22:36 +0700

    edit push

--- botpush.php
+++ botpush.php
@@ -11,7 +11,7 @@ $pushID = 'U1b80d09ffe5c7f746850ca99a023d30b';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Tum');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
