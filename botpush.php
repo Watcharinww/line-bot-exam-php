7a154ae24b972a029587eff536fa694e521826a1
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-01-08 13:25:43 +0700

    add tum

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ $access_token = 'UoNWCzm+34uMMB2itvPBwm7K9N7CK8GbWMc5RFmI9KQqGtAM1YO24VRTp5xTbzY
 
 $channelSecret = '88f693bafb5809e65f319ad3139213ba';
 
-$pushID = 'U1b80d09ffe5c7f746850ca99a023d30b';
+$pushID = 'Udfd683ae78963f854fcb0966fbe64ef6';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
