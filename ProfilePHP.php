4216724281f1d166f2e69daf0eb8bd49c7933fac
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-01-20 16:41:39 +0700

    edit Profile

--- ProfilePHP.php
+++ ProfilePHP.php
@@ -1,7 +1,5 @@
 <?php
 
-require "vendor/autoload.php";
-
 $access_token = 'UoNWCzm+34uMMB2itvPBwm7K9N7CK8GbWMc5RFmI9KQqGtAM1YO24VRTp5xTbzYk4jN9n0zEqc86nVJQTyIVJOimoI9CPzcuaCUyysOLMvBtooxc7BK6pfNYdRZ6mzobVVvb7/DlYxK/LdHddOHrrwdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '88f693bafb5809e65f319ad3139213ba';
