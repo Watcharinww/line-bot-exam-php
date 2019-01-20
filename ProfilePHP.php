21147e46b4643a5cbfd174619261e18946745f86
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-01-20 16:11:52 +0700

    Edit profile

--- ProfilePHP.php
+++ ProfilePHP.php
@@ -9,16 +9,16 @@ $channelSecret = '88f693bafb5809e65f319ad3139213ba';
 $userID = 'U1b80d09ffe5c7f746850ca99a023d30b';
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret ]);
-$response = $bot->getProfile($userID);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('$access_token');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '$channelSecret']);
+$response = $bot->getProfile('$userID');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
-    echo 'Username : '.$profile['displayName'].'<br>';
-    //echo "<br>";
-    echo 'PictureLink : '.$profile['pictureUrl'].'<br>';
-    echo 'MassageStatus : '.$profile['statusMessage'].<'<br>';
+    echo $profile['displayName'];
+    echo $profile['pictureUrl'];
+    echo $profile['statusMessage'];
 }
+
 
 exit; 
 ?>
