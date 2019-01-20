761025fa328529eff10425cf13eb94c866eb5e68
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-01-20 16:13:08 +0700

    edit profile

--- ProfilePHP.php
+++ ProfilePHP.php
@@ -9,9 +9,9 @@ $channelSecret = '88f693bafb5809e65f319ad3139213ba';
 $userID = 'U1b80d09ffe5c7f746850ca99a023d30b';
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('$access_token');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '$channelSecret']);
-$response = $bot->getProfile('$userID');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$response = $bot->getProfile($userID);
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
