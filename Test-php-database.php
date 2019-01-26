46a7e057c48d1547bd6c2ab67e3b20618101feaa
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-01-26 16:54:21 +0700

    edit check database

--- Test-php-database.php
+++ Test-php-database.php
@@ -21,7 +21,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["Id"]. " - Name: ". $row["Name"]. "<br>";
+        echo "<br> id: ". $row["Id"]. " - Name: ". $row["Name"]. "<br>"."Line ID: ".$row["LineId"];
     }
 } else {
     echo "0 results";
