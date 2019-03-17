2623b1b5441b990b46f21131fb9b80658c5c6388
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-03-17 17:56:27 +0700

    change position

--- testbotreply/echo_bot.php
+++ testbotreply/echo_bot.php
@@ -1,4 +1,4 @@
-<?php // callback.php
+<?php  // callback.php
 
 require "vendor/autoload.php";
 require 'conn.php';
@@ -17,71 +17,67 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 
-// Loop through each event
+    // Loop through each event
 
-foreach ($events['events'] as $event) {
+    foreach ($events['events'] as $event) {
 
-// Reply only when message sent is in 'text' format
+        // Reply only when message sent is in 'text' format
 
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-// Get text sent
+            // Get text sent
 
-$text = $event['source']['userId'];
-$text2 = $event['text'];
+            $text = $event['source']['userId'];
+            $text2 = $event['text'];
 
-// Get replyToken
+            // Get replyToken
 
-$replyToken = $event['replyToken'];
+            $replyToken = $event['replyToken'];
 
-// Build message to reply back
+            // Build message to reply back
 
-$messages = [
+            $messages = [
 
-'type' => 'text',
+                'type' => 'text',
 
-'text' => $text2
+                'text' => $text
 
-];
+            ];
+        }
+        // Make a POST Request to Messaging API to reply to sender
 
-// Make a POST Request to Messaging API to reply to sender
+        $url = 'https://api.line.me/v2/bot/message/reply';
 
-$url = 'https://api.line.me/v2/bot/message/reply';
+        $data = [
 
-$data = [
+            'replyToken' => $replyToken,
 
-'replyToken' => $replyToken,
+            'messages' => [$messages],
 
-'messages' => [$messages],
+        ];
 
-];
+        $post = json_encode($data);
 
-$post = json_encode($data);
+        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+        $ch = curl_init($url);
 
-$ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
 
-$result = curl_exec($ch);
+        curl_close($ch);
 
-curl_close($ch);
-
-echo $result . "\r\n";
-
-}
-
-}
-
+        echo $result . "\r\n";
+    }
 }
 
 echo "OK";
