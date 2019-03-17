4082b91265619dfb2ae100f1d35f0fd591333de0
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-03-17 23:48:32 +0700

    try

--- test_push.php
+++ test_push.php
@@ -1,28 +1,3 @@
-<!-- <?
-        require 'vendor/autoload.php';
-
-
-        $pushId = 'U1b80d09ffe5c7f746850ca99a023d30b';
-
-        use LINE\LINEBot\MessageBuilder\RawMessageBuilder;
-
-        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-        $text = [
-            'type' => 'text',
-            'text' => 'จะทดลองทำไมอะ'
-        ];
-
-        // $RawMessageBuilder = new RawMessageBuilder($text);
-        $response = $bot->pushMessage($pushId, new FlexMessageBuilder($text));
-
-        echo $response->getHTTPStatus();
-
-        $conn->close();
-        ?> -->
-
-
 <?php
 require 'conn.php';
 
