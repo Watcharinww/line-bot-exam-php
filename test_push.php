6de6a0d0dc5efc19a6cc9bbe78d5070d5dc887c8
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-03-18 00:43:08 +0700

    try

--- test_push.php
+++ test_push.php
@@ -1,77 +1,207 @@
 <?php
+/**
+ * Copyright 2018 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+namespace LINE\Tests\LINEBot;
 require 'conn.php';
 
-$accessToken = $access_token; //copy ข้อความ Channel access token ตอนที่ตั้งค่า
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\Flex\ComponentLayout;
+use LINE\LINEBot\MessageBuilder\RawMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+use LINE\Tests\LINEBot\Util\DummyHttpClient;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\BoxComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ButtonComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\IconComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\ImageComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\CarouselContainerBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+use LINE\LINEBot\Constant\Flex\ComponentIconSize;
+use LINE\LINEBot\Constant\Flex\ComponentImageSize;
+use LINE\LINEBot\Constant\Flex\ComponentImageAspectRatio;
+use LINE\LINEBot\Constant\Flex\ComponentImageAspectMode;
+use LINE\LINEBot\Constant\Flex\ComponentFontSize;
+use LINE\LINEBot\Constant\Flex\ComponentFontWeight;
+use LINE\LINEBot\Constant\Flex\ComponentMargin;
+use LINE\LINEBot\Constant\Flex\ComponentSpacing;
+use LINE\LINEBot\Constant\Flex\ComponentButtonStyle;
+use LINE\LINEBot\Constant\Flex\ComponentButtonHeight;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\SpacerComponentBuilder;
+use LINE\LINEBot\Constant\Flex\ComponentSpaceSize;
+use LINE\LINEBot\Constant\Flex\ComponentGravity;
+use LINE\LINEBot\QuickReplyBuilder\ButtonBuilder\QuickReplyButtonBuilder;
+use LINE\LINEBot\QuickReplyBuilder\QuickReplyMessageBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
-$content = file_get_contents('php://input');
-$arrayJson = json_decode($content, true);
-
-   $arrayHeader = array();
-   $arrayHeader[] = "Content-Type: application/json";
-   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-
-// $arrayHeader = array(
-//     'Content-Type : application/json',
-//     'Authorization : Bearer ' . $accessToken
-// );
-
-//รับข้อความจากผู้ใช้
-$message = $arrayJson['events'][0]['message']['text'];
-
-//รับ id ของผู้ใช้
-//    $id = $arrayJson['events'][0]['source']['userId'];
-
-#ตัวอย่าง Message Type "Text + Sticker"
-// if ($message == "สวัสดี") {
-    $message_sent['to'] = $pushId;
-    $message_sent['messages'][0]['type'] = "text";
-    $message_sent['messages'][0]['text'] = "สวัสดีจ้าาา";
-    $message_sent['messages'][1]['type'] = "sticker";
-    $message_sent['messages'][1]['packageId'] = "2";
-    $message_sent['messages'][1]['stickerId'] = "34";
-    // pushMsg($arrayHeader, $arrayPostData);
-// } else {
-//     $message_sent = [
-//         'to' => $id,
-//         'messages' => [
-//             'type' => 'text',
-//             'text' => 'สวัสดีไงหละะะะ อีกแบบอะ'
-//         ]
-//     ];
-//     // pushMsg($arrayHeader, $message);
-// }
-
-$url = 'https://api.line.me/v2/bot/message/push';
-
-$post = json_encode($message_sent);
-$ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $result = curl_exec($ch);
-      curl_close ($ch);
-
-
-// function pushMsg($arrayHeader, $arrayPostData)
-// {
-//     $strUrl = "https://api.line.me/v2/bot/message/push";
-
-//     $ch = curl_init();
-//     curl_setopt($ch, CURLOPT_URL, $strUrl);
-//     curl_setopt($ch, CURLOPT_HEADER, false);
-//     curl_setopt($ch, CURLOPT_POST, true);
-//     curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-//     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//     $result = curl_exec($ch);
-//     curl_close($ch);
-
-//     echo $result . "\r\n";
-// }
-
-// exit;
-?> 
+$pushId = 'U1b80d09ffe5c7f746850ca99a023d30b';
+$httpClient = new CurlHTTPClient($access_token);
+$bot = new LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+        $res = $bot->pushMessage(
+            $pushId,
+            FlexMessageBuilder::builder()
+                ->setAltText('Shopping')
+                ->setContents(
+                    CarouselContainerBuilder::builder()
+                        ->setContents([
+                            BubbleContainerBuilder::builder()
+                                ->setHero(
+                                    ImageComponentBuilder::builder()
+                                        ->setSize(ComponentImageSize::FULL)
+                                        ->setAspectRatio(ComponentImageAspectRatio::R20TO13)
+                                        ->setAspectMode(ComponentImageAspectMode::COVER)
+                                        ->setUrl('https://example.com/photo1.png')
+                                )
+                                ->setBody(
+                                    BoxComponentBuilder::builder()
+                                        ->setLayout(ComponentLayout::VERTICAL)
+                                        ->setSpacing(ComponentSpacing::SM)
+                                        ->setContents([
+                                            TextComponentBuilder::builder()
+                                                ->setText('Arm Chair, White')
+                                                ->setWrap(true)
+                                                ->setWeight(ComponentFontWeight::BOLD)
+                                                ->setSize(ComponentFontSize::XL),
+                                            BoxComponentBuilder::builder()
+                                                ->setLayout(ComponentLayout::BASELINE)
+                                                ->setContents([
+                                                    TextComponentBuilder::builder()
+                                                        ->setText('$49')
+                                                        ->setWrap(true)
+                                                        ->setWeight(ComponentFontWeight::BOLD)
+                                                        ->setSize(ComponentFontSize::XL)
+                                                        ->setFlex(0),
+                                                    TextComponentBuilder::builder()
+                                                        ->setText('.99')
+                                                        ->setWrap(true)
+                                                        ->setWeight(ComponentFontWeight::BOLD)
+                                                        ->setSize(ComponentFontSize::SM)
+                                                        ->setFlex(0)
+                                                ])
+                                        ])
+                                )
+                                ->setFooter(
+                                    BoxComponentBuilder::builder()
+                                        ->setLayout(ComponentLayout::VERTICAL)
+                                        ->setSpacing(ComponentSpacing::SM)
+                                        ->setContents([
+                                            ButtonComponentBuilder::builder()
+                                                ->setStyle(ComponentButtonStyle::PRIMARY)
+                                                ->setAction(
+                                                    new UriTemplateActionBuilder(
+                                                        'Add to Cart',
+                                                        'https://example.com'
+                                                    )
+                                                ),
+                                            ButtonComponentBuilder::builder()
+                                                ->setAction(
+                                                    new UriTemplateActionBuilder(
+                                                        'Add to wishlist',
+                                                        'https://example.com'
+                                                    )
+                                                )
+                                        ])
+                                ),
+                            BubbleContainerBuilder::builder()
+                                ->setHero(
+                                    ImageComponentBuilder::builder()
+                                        ->setSize(ComponentImageSize::FULL)
+                                        ->setAspectRatio(ComponentImageAspectRatio::R20TO13)
+                                        ->setAspectMode(ComponentImageAspectMode::COVER)
+                                        ->setUrl('https://example.com/photo2.png')
+                                )
+                                ->setBody(
+                                    BoxComponentBuilder::builder()
+                                        ->setLayout(ComponentLayout::VERTICAL)
+                                        ->setSpacing(ComponentSpacing::SM)
+                                        ->setContents([
+                                            TextComponentBuilder::builder()
+                                                ->setText('Metal Desk Lamp')
+                                                ->setWrap(true)
+                                                ->setWeight(ComponentFontWeight::BOLD)
+                                                ->setSize(ComponentFontSize::XL),
+                                            BoxComponentBuilder::builder()
+                                                ->setLayout(ComponentLayout::BASELINE)
+                                                ->setContents([
+                                                    TextComponentBuilder::builder()
+                                                        ->setText('$11')
+                                                        ->setWrap(true)
+                                                        ->setWeight(ComponentFontWeight::BOLD)
+                                                        ->setSize(ComponentFontSize::XL)
+                                                        ->setFlex(0),
+                                                    TextComponentBuilder::builder()
+                                                        ->setText('.99')
+                                                        ->setWrap(true)
+                                                        ->setWeight(ComponentFontWeight::BOLD)
+                                                        ->setSize(ComponentFontSize::SM)
+                                                        ->setFlex(0)
+                                                ]),
+                                            TextComponentBuilder::builder()
+                                                ->setText('Temporarily out of stock')
+                                                ->setWrap(true)
+                                                ->setSize(ComponentFontSize::XXS)
+                                                ->setMargin(ComponentMargin::MD)
+                                                ->setColor('#ff5551')
+                                                ->setFlex(0)
+                                        ])
+                                )
+                                ->setFooter(
+                                    BoxComponentBuilder::builder()
+                                        ->setLayout(ComponentLayout::VERTICAL)
+                                        ->setSpacing(ComponentSpacing::SM)
+                                        ->setContents([
+                                            ButtonComponentBuilder::builder()
+                                                ->setStyle(ComponentButtonStyle::PRIMARY)
+                                                ->setColor('#aaaaaa')
+                                                ->setAction(
+                                                    new UriTemplateActionBuilder(
+                                                        'Add to Cart',
+                                                        'https://example.com'
+                                                    )
+                                                ),
+                                            ButtonComponentBuilder::builder()
+                                                ->setAction(
+                                                    new UriTemplateActionBuilder(
+                                                        'Add to wishlist',
+                                                        'https://example.com'
+                                                    )
+                                                )
+                                        ])
+                                ),
+                            BubbleContainerBuilder::builder()
+                                ->setBody(
+                                    BoxComponentBuilder::builder()
+                                        ->setLayout(ComponentLayout::VERTICAL)
+                                        ->setSpacing(ComponentSpacing::SM)
+                                        ->setContents([
+                                            ButtonComponentBuilder::builder()
+                                                ->setFlex(1)
+                                                ->setGravity(ComponentGravity::CENTER)
+                                                ->setAction(
+                                                    new UriTemplateActionBuilder(
+                                                        'See more',
+                                                        'https://example.com'
+                                                    )
+                                                )
+                                        ])
+                                )
+                        ])
+                )
+        );
+        $this->assertEquals(200, $res->getHTTPStatus());
+        $this->assertTrue($res->isSucceeded());
+        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
