e95c9161f09f3e2029b83be649da75acb381114a
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-03-19 02:28:26 +0700

    try

--- botbroadcast.php
+++ botbroadcast.php
@@ -24,6 +24,13 @@ while ($row = mysqli_fetch_array($result)) {
         $response = $bot->pushMessage($row["std_l_id"], $textMessageBuilder);
     }
 }
+$status = $response->getHTTPStatus();
+
+if(($status) == 200){
+    echo "<script> window.alert('broadcast สำเร็จ'); window.location.href = 'index.php';</script>"; 
+}else{
+    echo "<script> window.alert('มีปัญหาในการ broadcast'); window.location.href = 'index.php';</script>"; 
+}
 
 $conn->close();
 
