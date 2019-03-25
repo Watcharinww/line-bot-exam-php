f81bd8dfd15a4f0fa15f523beaf08d4021871f81
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-03-25 23:38:04 +0700

    add std_y

--- register.php
+++ register.php
@@ -58,6 +58,7 @@ while ($row = $result->fetch_assoc()) {
                             <option value=2>2</option>
                             <option value=3>3</option>
                             <option value=4>4</option>
+                            <option value=5>5</option>
                         </select><br>
                     </td>
                 <tr hidden>
