60e1720bd11b3149889ebb66148ec8926458edd0
Watcharinww <42956626+Watcharinww@users.noreply.github.com>
2019-05-18 23:06:40 +0700

    edit name

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
 
 
         <td class='text-center align-middle' rowspan='2'>
-            <a class='h1 text-white  web-breting'>Welcome to Line-LMS</a><br><br>
+            <a class='h1 text-white  web-breting'>Welcome to Support Line-LMS</a><br><br>
             <img class='web-border' src="http://qr-official.line.me/L/1q8xh1Rd96.png">
         </td>
     </tr>
